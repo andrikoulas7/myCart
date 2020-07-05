c92bd9bdb422d2aaa786dd441e517b7e2056fadd
andrikoulas7 <panagandrea@gmail.com>
2020-07-05 23:52:12 +0200

    Sticky div added

--- storeApp.js
+++ storeApp.js
@@ -1,14 +1,14 @@
 var app = angular.module('myApp', []);
 app.controller('productsController', ['$scope', function ($scope) {
     $scope.products = [
-        { name: "Oranges", category: "Citrus", price: 300 },
-        { name: "Grapefruits", category: "Citrus", price: 300 },
+        { name: "Oranges", category: "Citrus", price: 300, image: "./images/oranges.jpg" },
+        { name: "Grapefruits", category: "Citrus", price: 300, image: "./images/grapefruit.jpg" },
         { name: "Mandarins", category: "Citrus", price: 500 },
         { name: "Limes", category: "Citrus", price: 600 },
         { name: "Nectarines", category: "Stone fruit", price: 700 },
         { name: "Apricots", category: "Stone fruit", price: 250 },
         { name: "Peach", category: "Stone fruit", price: 350 },
-        { name: "Bananas", category: "Tropical", price: 100 },
+        { name: "Bananas", category: "Tropical", price: 100, image: "./images/bananas.jpg" },
         { name: "Mangoes", category: "Tropical", price: 550 },
         { name: "Strawberries", category: "Berries", price: 160 },
         { name: "Blueberries", category: "Berries", price: 700 },
@@ -18,6 +18,7 @@ app.controller('productsController', ['$scope', function ($scope) {
     $scope.cart = [];
     $scope.total = 0;
 
+    //add products to your cart
     $scope.addtoCart = function (price, name, quantity) {
         $scope.cart.push({
             name: name,
@@ -31,6 +32,7 @@ app.controller('productsController', ['$scope', function ($scope) {
         $scope.product = name;
     }
 
+    //remove products from your cart
     $scope.removeCart = function (index, timi, posotita) {
         //var index = $scope.cart.indexOf(posotita);
         $scope.cart.splice(index, 1);
