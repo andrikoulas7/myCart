4422ca972a0a6a9400ebbdf5cfdb7e6ec657feab
andrikoulas7 <panagandrea@gmail.com>
2020-07-06 18:40:58 +0200

    Clear cart added

--- storeApp.js
+++ storeApp.js
@@ -1,17 +1,17 @@
 var app = angular.module('myApp', []);
 app.controller('productsController', ['$scope', function ($scope) {
     $scope.products = [
-        { name: "Oranges", category: "Citrus", price: 300, image: "./images/oranges.jpg" },
-        { name: "Grapefruits", category: "Citrus", price: 300, image: "./images/grapefruit.jpg" },
-        { name: "Mandarins", category: "Citrus", price: 500 },
-        { name: "Limes", category: "Citrus", price: 600 },
-        { name: "Nectarines", category: "Stone fruit", price: 700 },
-        { name: "Apricots", category: "Stone fruit", price: 250 },
-        { name: "Peach", category: "Stone fruit", price: 350 },
-        { name: "Bananas", category: "Tropical", price: 100, image: "./images/bananas.jpg" },
-        { name: "Mangoes", category: "Tropical", price: 550 },
-        { name: "Strawberries", category: "Berries", price: 160 },
-        { name: "Blueberries", category: "Berries", price: 700 },
+        { name: "Oranges", category: "Citrus", price: 30, image: "./images/oranges.jpg" },
+        { name: "Grapefruits", category: "Citrus", price: 30, image: "./images/grapefruit.jpg" },
+        { name: "Mandarins", category: "Citrus", price: 18 },
+        { name: "Limes", category: "Citrus", price: 25 },
+        { name: "Nectarines", category: "Stone fruit", price: 24 },
+        { name: "Apricots", category: "Stone fruit", price: 25 },
+        { name: "Peach", category: "Stone fruit", price: 35 },
+        { name: "Bananas", category: "Tropical", price: 10, image: "./images/bananas.jpg" },
+        { name: "Mangoes", category: "Tropical", price: 21 },
+        { name: "Strawberries", category: "Berries", price: 16 },
+        { name: "Blueberries", category: "Berries", price: 14 },
 
     ];
 
@@ -46,12 +46,18 @@ app.controller('productsController', ['$scope', function ($scope) {
 
     }
 
-    $scope.clearCart = function (onoma, posotita, timi) {
+    $scope.clearCart = function () {
         $scope.total = 0;
-        $scope.name = "";
-        $scope.posotita = 0;
-        $scope.timi = 0;
+        var len = $scope.cart.length;
+        console.log(len);
+        $scope.cart.splice(0,len);
+        //$scope.cart.push({
+        //    name: "",
+        //    posotita: 0,
+        //    timi: 0
+        //})
         console.dir($scope.cart);
+        console.log($scope.cart);
 
     }
 
