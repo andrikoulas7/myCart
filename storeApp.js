7581dbe3089484fd98d1e122c5e66f90ee403a71
andrikoulas7 <panagandrea@gmail.com>
2020-07-08 00:12:08 +0200

    Updates including clear cart button

--- storeApp.js
+++ storeApp.js
@@ -3,15 +3,15 @@ app.controller('productsController', ['$scope', function ($scope) {
     $scope.products = [
         { name: "Oranges", category: "Citrus", price: 30, image: "./images/oranges.jpg" },
         { name: "Grapefruits", category: "Citrus", price: 30, image: "./images/grapefruit.jpg" },
-        { name: "Mandarins", category: "Citrus", price: 18 },
-        { name: "Limes", category: "Citrus", price: 25 },
-        { name: "Nectarines", category: "Stone fruit", price: 24 },
-        { name: "Apricots", category: "Stone fruit", price: 25 },
-        { name: "Peach", category: "Stone fruit", price: 35 },
+        { name: "Mandarins", category: "Citrus", price: 18, image: "./images/mandarin.jpg" },
+        { name: "Limes", category: "Citrus", price: 25, image: "./images/lime.jpg" },
+        { name: "Nectarines", category: "Stone fruit", price: 24, image: "./images/nectarin.jpg" },
+        { name: "Apricots", category: "Stone fruit", price: 25, image: "./images/apricot.jpg" },
+        { name: "Peach", category: "Stone fruit", price: 35, image: "./images/peach.jpg" },
         { name: "Bananas", category: "Tropical", price: 10, image: "./images/bananas.jpg" },
-        { name: "Mangoes", category: "Tropical", price: 21 },
-        { name: "Strawberries", category: "Berries", price: 16 },
-        { name: "Blueberries", category: "Berries", price: 14 },
+        { name: "Mangoes", category: "Tropical", price: 21, image: "./images/mangos.jpg" },
+        { name: "Strawberries", category: "Berries", price: 16, image: "./images/strawberries.jpg" },
+        { name: "Blueberries", category: "Berries", price: 14, image: "./images/blueberries.jpg" },
 
     ];
 
@@ -20,16 +20,24 @@ app.controller('productsController', ['$scope', function ($scope) {
 
     //add products to your cart
     $scope.addtoCart = function (price, name, quantity) {
-        $scope.cart.push({
-            name: name,
-            posotita: quantity,
-            timi: price
-        });
-        //console.dir(name);
+        if (quantity == 0) {
+            $scope.cart.push({
+                name: name,
+                posotita: 1,
+                timi: price
+            })
+        } else {
+            $scope.cart.push({
+                name: name,
+                posotita: quantity,
+                timi: price
+            });
+        }        //console.dir(name);
         console.dir($scope.cart);
         $scope.total += (price * quantity);
         $scope.posotita = quantity;
         $scope.product = name;
+        console.log("Posotita: ",quantity);
     }
 
     //remove products from your cart
@@ -46,11 +54,12 @@ app.controller('productsController', ['$scope', function ($scope) {
 
     }
 
+    //Remove all products from cart
     $scope.clearCart = function () {
         $scope.total = 0;
         var len = $scope.cart.length;
         console.log(len);
-        $scope.cart.splice(0,len);
+        $scope.cart.splice(0, len);
         //$scope.cart.push({
         //    name: "",
         //    posotita: 0,
